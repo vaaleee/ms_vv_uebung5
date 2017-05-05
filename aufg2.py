c9f3708ae225377274aab4077759ecd36b9f9afc
vaaleee <valentina.vogel@hotmail.com>
2017-05-05 14:42:45 +0200

    Aufgabe 2 eigentlich fertig.

--- aufg2.py
+++ aufg2.py
@@ -35,21 +35,55 @@ class NaiveBayesClassifierNameGenderPrediction:
         # overall classifier accuracy, as well as precision, recall and
         # f-measure for male and female
 
+        accuracy_ = accuracy(classifier, test_set)
+   
+        refsets = collections.defaultdict(set)
+        testsets = collections.defaultdict(set)
+ 
+        for index, (feats, label) in enumerate(test_set):
+            refsets[label].add(index)
+            observed = classifier.classify(feats)
+            testsets[observed].add(index)
+ 
+        print('male precision:', precision(refsets['male'], testsets['male']))
+        print('male recall:', recall(refsets['male'], testsets['male']))
+        print('male F-measure:', f_measure(refsets['male'], testsets['male']))
+        print('female precision:', precision(refsets['female'], testsets['female']))
+        print('female recall:', recall(refsets['female'], testsets['female']))
+        print('female F-measure:', f_measure(refsets['female'], testsets['female']))
+        print('accuracy:', accuracy_)
+
+
+         
+
 
         
     @staticmethod
     def gender_features(name):
         """Return a dictionary with all features to identify the gender of
         a name"""
-        return {
+
+        feature_dict = {
             'ends_with_a': True if name.endswith('a') else False,
-            #'first_char': name[0],
+            
             'long_name': True if len(name) > 8 else False,
             'ends_with_r': True if name.endswith('r') else False,
-            'contains_ll': True if 'll' in name else False,
+            #'contains_ll': True if 'll' in name else False,
             'ends_with_o': True if name.endswith('o') else False,
+            'ends_with_k': True if name.endswith('k') else False,
+            #'ends_with_p': True if name.endswith('p') else False,
             'short_name': True if len(name) < 5 else False,
+            'ends_with_s': True if name.endswith('s') else False,
         }
+
+        if len(name) >0:
+            feature_dict['first_char'] = name[0]
+            feature_dict['last_char'] = name[-1]
+
+        if len(name) >3:
+            feature_dict['last_three'] = name[-3]
+
+        return feature_dict
         # TODO: Add further features to maximise the classifier's performance.
 
     def get_training_and_test_labeled_features(self, female_training_data,
@@ -74,7 +108,7 @@ class NaiveBayesClassifierNameGenderPrediction:
         for female_name in female_training_data:
             train_dict[female_name] = 'female'
 
-        for name in train_dict:
+        for name in train_dict.keys():
             train_feature_list.append((self.gender_features(name), train_dict[name]))
 
         for male_name in male_test_data:
@@ -134,6 +168,8 @@ class NaiveBayesClassifierNameGenderPrediction:
             self.get_train_and_test_data(male_data, female_data)
 
 
+
+
         # get the training and test set for the classifier and the evaluation
         train_set, test_set = self.get_training_and_test_labeled_features(
             female_train_data, male_train_data, female_test_data,
@@ -142,12 +178,17 @@ class NaiveBayesClassifierNameGenderPrediction:
 
         # create classifier with the training set
         classifier = NaiveBayesClassifier.train(train_set)
+        # erste Tests:
+        #print('Shrek: \t', classifier.classify(self.gender_features('Shrek')))
+        #print('Annabelle: \t', classifier.classify(self.gender_features('Annabelle')))
+        #print('Klo: \t', classifier.classify(self.gender_features('Marco')))
+        #print('Muellabfuhr: \t', classifier.classify(self.gender_features('Cheryl')))
 
         # print the evaluation with the precision, recall and f-measure
-        #self.evaluation(test_set, classifier)
+        self.evaluation(test_set, classifier)
         
         # print the 10 most informative features 
-        #classifier.show_most_informative_features(1)
+        # classifier.show_most_informative_features(10)
 
 
 if __name__ == '__main__':
@@ -158,3 +199,4 @@ if __name__ == '__main__':
 
 
 # http://stackoverflow.com/questions/17412439/how-to-split-data-into-trainset-and-testset-randomly
+# http://www.nltk.org/book/ch06.html
